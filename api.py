6fe1404788e8d6f5130a5cc3194dca03cc49f6dc
Gustavo Henriquez <gustavo.henriquez.m@gmail.com>
2022-12-30 00:38:17 -0300

    Update api.py
    asd

--- api.py
+++ api.py
@@ -3,7 +3,16 @@ from reader import getSentimiento
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/{busqueda}")
 async def root(busqueda):
     salida=getSentimiento(busqueda)
